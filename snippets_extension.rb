f46acda022a1ee9b29147fa40290cc8d4fb39f67
Brian Quinn <briandquinn@gmail.com>
2010-01-18 10:52:54 +0000

    Removed exceptions when snippets cannot be found

--- snippets_extension.rb
+++ snippets_extension.rb
@@ -2,7 +2,7 @@ class SnippetsExtension < Spree::Extension
   version "0.2"
   description "Admin configurable static content for CMS control over specific sections of a view."
   url "http://github.com/cmaujean/spree-snippets"
-  
+
   # Developer will embed calls to render specific snippet slugs or ids
   # in any view. Admin will control content via /admin/snippets.
   def activate
@@ -10,24 +10,28 @@ class SnippetsExtension < Spree::Extension
     Spree::BaseController.class_eval do
 
       helper_method :render_snippet
-      
-      # snippet can be something that responds to "slug" and "content", 
+
+      # snippet can be something that responds to "slug" and "content",
       # or a slug, or an id
       def render_snippet(snippet)
-        if snippet.respond_to?('content')  
+        if snippet.respond_to?('content')
           @snippet = snippet
         elsif snippet.kind_of?(Fixnum)
           @snippet = Snippet.find(snippet)
         elsif snippet.kind_of?(String)
           @snippet = Snippet.find_by_slug(snippet)
         else
-          raise "Unable to handle snippet '#{snippet}'"
-        end 
-        raise "Snippet '#{snippet}' not found" if @snippet.nil?
-        template = ERB.new File.read(File.expand_path(snippet_wrapper_absolute_path))
-        template.result(binding)
+          "<p style='color:red;'>Unable to handle snippet '#{snippet}'</p>"
+        end
+
+        if @snippet.nil?
+          "<p style='color:red;'>Snippet '#{snippet}' not found</p>"
+        else
+          template = ERB.new File.read(File.expand_path(snippet_wrapper_absolute_path))
+          template.result(binding)
+        end
       end
-      
+
       private
       # Returns the location of the snippet wrapper, which is dependent on how spree is installed.
       def snippet_wrapper_absolute_path
@@ -35,7 +39,7 @@ class SnippetsExtension < Spree::Extension
           absolute_path = root + snippet_wrapper_path
           return absolute_path if File.exists?(absolute_path)
         end
-        raise "Unable to find snippet wrapper using path '#{snippet_wrapper_path}'"        
+        raise "Unable to find snippet wrapper using path '#{snippet_wrapper_path}'"
       end
       # Override the location returned by this method if you want your own snippet wrapper template
       def snippet_wrapper_path
